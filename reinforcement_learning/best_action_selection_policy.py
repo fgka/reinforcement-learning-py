946549ea48465915f8b9a1f831f1476941c528b7
fgka <gustavo.andriotti@gmail.com>
2018-05-20 13:41:55 -0700

    Added documentation and fixed all lint warnings to BestActionSelectionPolicy.

--- reinforcement_learning/best_action_selection_policy.py
+++ reinforcement_learning/best_action_selection_policy.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # vim: ai:sw=4:ts=4:sta:et:fo=croql
 # coding=utf-8
+""" .. py:currentmodule:: best_action_selection_policy
+
+Definition of :py:class:`BestActionSelectionPolicy`.
+"""
 
 import operator
 import math
@@ -20,29 +24,32 @@ class BestActionSelectionPolicy(QActionSelectionPolicy):
 
     def select(self, action_values: dict) -> QAction:
         result = None
-        if action_values is not None and len(action_values) > 0:
+        if action_values is not None and action_values:
             result = self._safe_select(action_values)
         return result
 
     def _safe_select(self, action_values: dict) -> QAction:
         result = None
         if self._randomize:
-            result = self._select_random_max(action_values)
+            result = _select_random_max(action_values)
         else:
-            result = self._select_non_random_max(action_values)
+            result = _select_non_random_max(action_values)
         return result
 
-    def _select_random_max(self, action_values: dict) -> QAction:
-        max_value = self._get_max_value_action_value_pair(action_values)[1]
-        return random.choice([action
-                              for action, value in action_values.items()
-                              if math.isclose(value, max_value)])
 
-    def _select_non_random_max(self, action_values: dict) -> QAction:
-        return self._get_max_value_action_value_pair(action_values)[0]
+def _select_random_max(action_values: dict) -> QAction:
+    max_value = _get_max_value_action_value_pair(action_values)[1]
+    return random.choice([action
+                          for action, value in action_values.items()
+                          if math.isclose(value, max_value)])
 
-    def _get_max_value_action_value_pair(self, action_values: dict) -> tuple:
-        return max(action_values.items(), key=operator.itemgetter(1))
+
+def _select_non_random_max(action_values: dict) -> QAction:
+    return _get_max_value_action_value_pair(action_values)[0]
+
+
+def _get_max_value_action_value_pair(action_values: dict) -> tuple:
+    return max(action_values.items(), key=operator.itemgetter(1))
 
 
 # breaks ties randomly
